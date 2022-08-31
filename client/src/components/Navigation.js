ac506f2624fd64ce520478596c3f1691af40d6da
Mark Pavel <markpavel02@gmail.com>
2022-08-31 19:49:14 -0400

    Fixed Router

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import Container from "react-bootstrap/Container"
+import { Link } from "react-router-dom"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 
@@ -6,13 +7,13 @@ const Navigation = (props) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Instaclone-</Navbar.Brand>
+        <Navbar.Brand as={Link} to="#home">Instaclone-</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/users">Following</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/users">Following</Nav.Link>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
