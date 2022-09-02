56ba0c50b996508873ec586c849eb13337246e1b
rstanbaugh <rich_bootcamp@icloud.com>
2022-09-01 21:51:34 -0400

    createPost

--- server/controllers/post-controller.js
+++ server/controllers/post-controller.js
@@ -1,94 +1,102 @@
-const { Post, User, Comment } = require('../models');
+const { Post } = require('../models');
 
 
-const postController = {
+
   // get all posts
-  getAllPosts(req, res) {
-    Post.find({})
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .populate({
-        path: 'users',
-        select: '-__v'
-      })
-      .select('-__v')
-      .sort({ _id: -1 })
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => {
-        console.log(err);
-        res.status(400).json({ message: 'No posts found' });
-      });
-  },
+  // getAllPosts(req, res) {
+  //   Post.find({})
+  //     .populate({
+  //       path: 'comments',
+  //       select: '-__v'
+  //     })
+  //     .populate({
+  //       path: 'users',
+  //       select: '-__v'
+  //     })
+  //     .select('-__v')
+  //     .sort({ _id: -1 })
+  //     .then(dbPostData => res.json(dbPostData))
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.status(400).json({ message: 'No posts found' });
+  //     });
+  // },
 
-    // get one Post by id
-    getPostById({ params }, res) {
-      Post.findOne({ _id: params.id })
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .populate({
-        path: 'users',
-        select: '-__v'
-      })
-      .select('-__v')
-      .then(dbPostData => {
-          // If no post is found, send 404
-          if (!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id!' });
-            return;
-          }
-          res.json(dbPostData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+  //   // get one Post by id
+  //   getPostById({ params }, res) {
+  //     Post.findOne({ _id: params.id })
+  //     .populate({
+  //       path: 'comments',
+  //       select: '-__v'
+  //     })
+  //     .populate({
+  //       path: 'users',
+  //       select: '-__v'
+  //     })
+  //     .select('-__v')
+  //     .then(dbPostData => {
+  //         // If no post is found, send 404
+  //         if (!dbPostData) {
+  //           res.status(404).json({ message: 'No post found with this id!' });
+  //           return;
+  //         }
+  //         res.json(dbPostData);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //         res.status(400).json(err);
+  //       });
+  //   },
 
-    // get one posts by user
-    getPostsByCreator({ params }, res) {
-      Post.find({createdBy: params.createdBy })
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .populate({
-        path: 'users',
-        select: '-__v'
-      })
-      .select('-__v')
-      .then(dbPostData => {
-          // If no post is found, send 404
-          if (!dbPostData) {
-            res.status(404).json({ message: 'No post found fromthis user!' });
-            return;
-          }
-          res.json(dbPostData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+  //   // get one posts by user
+  //   getPostsByCreator({ params }, res) {
+  //     Post.find({createdBy: params.createdBy })
+  //     .populate({
+  //       path: 'comments',
+  //       select: '-__v'
+  //     })
+  //     .populate({
+  //       path: 'users',
+  //       select: '-__v'
+  //     })
+  //     .select('-__v')
+  //     .then(dbPostData => {
+  //         // If no post is found, send 404
+  //         if (!dbPostData) {
+  //           res.status(404).json({ message: 'No post found fromthis user!' });
+  //           return;
+  //         }
+  //         res.json(dbPostData);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //         res.status(400).json(err);
+  //       });
+  //   },
   // createPost
-  createPost({ body }, res) {
-    Post.create(body)
-      .then(dbPostData => res.json(dbPostData))
-      .catch(err => res.status(400).json(err));
-  },
-  // delete post
-  deletePost({ params }, res) {
-    Post.findOneAndDelete({ _id: params.id })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found with this id!' });
-          return;
-        }
-        res.json(dbPostData);
-      })
-      .catch(err => res.status(400).json(err));
+  // createPost({ body }, res) {
+  //   Post.create(body)
+  //     .then(dbPostData => res.json(dbPostData))
+  //     .catch(err => res.status(400).json(err));
+  // },
+  const createPost = async ({ body }, res) => {
+    await Post.create(body)
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.status(400).json({ message: 'Unable to create post' }));
   }
+  // delete post
+//   deletePost({ params }, res) {
+//     Post.findOneAndDelete({ _id: params.id })
+//       .then(dbPostData => {
+//         if (!dbPostData) {
+//           res.status(404).json({ message: 'No post found with this id!' });
+//           return;
+//         }
+//         res.json(dbPostData);
+//       })
+//       .catch(err => res.status(400).json(err));
+//   }
+
+module.exports = {
+  createPost
 }
--- server/routes/api/post-routes.js
+++ server/routes/api/post-routes.js
@@ -2,28 +2,29 @@ const router = require('express').Router();
 
 const {
   createPost, 
-  deletePost,
-  getAllPosts,
-  getPostById,
-  getPostsByCreator,
-  likePost
+  // deletePost,
+  // getAllPosts,
+  // getPostById,
+  // getPostsByCreator,
+  // likePost
 } = require('../../controllers/post-controller.js');
 
 // Declare the routes that point to the controllers above
 router.route('/')
-  .get(getAllPosts)
   .post(createPost)
+  // .get(getAllPosts)
+
 
   
-router.route("/lookup")
-  .get(getPostsByCreator)
+// router.route("/lookup")
+//   .get(getPostsByCreator)
 
-router.route('/:id')
-  .get(getPostById)
-  .delete(deletePost)
-  .put(likePost)
+// router.route('/:id')
+//   .get(getPostById)
+//   .delete(deletePost)
+//   .put(likePost)
 
 
-router.route('/:id/comment')
+// router.route('/:id/comment')
 
 module.exports = router
