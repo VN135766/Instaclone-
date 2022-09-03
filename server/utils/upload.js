dad0299fa5c23884595fec9c8d4952a4d7cddb17
Mark Pavel <markpavel02@gmail.com>
2022-09-03 13:06:03 -0400

    Added Uploading Images

---
+++ server/utils/upload.js
@@ -0,0 +1,14 @@
+var multer = require('multer');
+  
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+  
+var upload = multer({ storage: storage });
+
+module.exports = upload;
