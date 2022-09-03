9e74160208cbb9b32337407910a3b210d1999bce
MIshmail <michaely.ishmail@gmail.com>
2022-09-02 20:30:12 -0400

    added post and basic styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Post from './Post'
 
 const Home = (props) => {
   
   return (
     <div>
-      <h1>Hello!</h1>
+      <h1 className="text-center">Welcome to Instaclone!</h1>
 
       { props.authUser && props.authUser.email !== undefined && (
         <p>We have a logged in user: { props.authUser.email } </p>
       )}
+      <section>
+
+        <Post username={'Placeholder'} caption={"Placeholder caption"} imageurl={"https://cdn-media-1.freecodecamp.org/images/1*qUlxDdY3T-rDtJ4LhLGkEg.png"}/>
+        <Post username={'Placeholder'} caption={"Placeholder caption"} imageurl={"https://cdn-media-1.freecodecamp.org/images/1*qUlxDdY3T-rDtJ4LhLGkEg.png"}/>
+        <Post username={'Placeholder'} caption={"Placeholder caption"} imageurl={"https://cdn-media-1.freecodecamp.org/images/1*qUlxDdY3T-rDtJ4LhLGkEg.png"}/>
+        
+      </section>
     </div>
   )
 }
---
+++ client/src/pages/Post.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../post.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Post({ username, caption, imageurl }) {
+    return (
+        <div className='post'>
+            <h3 className='post_username'>{username}</h3>
+
+            <img className='post_img' src={imageurl}></img>
+
+            <h4 className='post_comment'><strong>{username}: </strong>{caption}</h4>
+        </div>
+    )
+}
+
+// function Post() {
+//     return (
+//       <Card style={{ width: '25rem' }}>
+
+//         <Card.Body>
+//         <Card.Title>Username</Card.Title>
+//         <Card.Img variant="top" src="https://cdn-media-1.freecodecamp.org/images/1*qUlxDdY3T-rDtJ4LhLGkEg.png" />
+//           <Card.Text>
+//             Post description
+//           </Card.Text>
+//           <Card.Text muted>
+//               Comments
+//           </Card.Text>
+//           <Card.text>
+//               Username
+//           </Card.text>
+//         </Card.Body>
+//       </Card>
+//     );
+//   }
+
+export default Post
