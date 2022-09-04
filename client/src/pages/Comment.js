d80a65c6c13955f516d30e2a93d5337fd04e5658
MIshmail <michaely.ishmail@gmail.com>
2022-09-03 20:54:13 -0400

    fixed an issue where the routes werent working properly

--- client/src/pages/Comment.js
+++ client/src/pages/Comment.js
@@ -4,8 +4,8 @@ import '../comments.css'
 function Comment({ username, caption }) {
     return (
         <div className='comment_container'>
-        <div className='comment'>
-            <h3 className='comment_username'>{username}</h3>
+        <div className='comments'>
+            <h4 className='comment_username'>{username}</h4>
 
             <h4 className='comment_caption'><strong>{username}: </strong>{caption}</h4>
         </div>
